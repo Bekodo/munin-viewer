b4d4c67f894e8da922896f735ff7ca4ffcfbb907
Systems Bekodo <systems@bekodo.com>
2017-08-03 16:50:31 +0200

    Add discard servers

--- functions.py
+++ functions.py
@@ -1,16 +1,32 @@
 import glob
 
-def getChars():
+def toFind(service):
+    services = { 'cpu': "aws*CPUutilization-day.png",
+                'memory': "*memory-day.png"  
+                }
+    return services.get(service,"nothing")
+
+def siteDiscard():
+    with open('sites.cfg') as file:
+        sites = file.read().split(",")
+        return sites
+
+def getChars(service):
     chars = []
+    charspath = "/var/www/html/munin/*/*/"
     index = 0
     aux = 0
     chars.append([])
-    for char in glob.iglob("/var/www/html/munin/*/*/aws*CPUutilization-day.png"):
+    sdiscard = siteDiscard()
+    pattern = toFind(service)
+    for char in glob.iglob(charspath + pattern):
         d = char.split('/')
         values = {'site': d[5], 'service': d[6], 'char': d[7] }
-        chars[index].append(values)
-        aux += 1
-        if (aux % 4) == 0:
-            index += 1
-            chars.append([])
+        print(d[6],sdiscard)
+        if not d[6] in sdiscard:
+            chars[index].append(values)
+            aux += 1
+            if (aux % 4) == 0:
+                index += 1
+                chars.append([])
     return chars
--- main.py
+++ main.py
@@ -3,10 +3,11 @@ import functions
 
 app = Flask(__name__)
 
-@app.route('/cpu')
-def cpu():
-    chars = functions.getChars()
-    return render_template('table.html', chars=chars)
+@app.route('/viewer/')
+@app.route('/viewer/<service>')
+def service(service="cpu"):
+    chars = functions.getChars(service)
+    return render_template('table.html', chars=chars, service=service)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0',debug=False)
+    app.run(host='0.0.0.0',debug=True)
