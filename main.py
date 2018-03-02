8437da417c406f31ca7d4bbd0f9bef51be6b622e
Hector Solans <hectors@gmail.com>
2018-03-02 10:53:15 +0100

    Change static files URL path

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 import functions
 
-app = Flask(__name__)
+app = Flask(__name__,static_url_path='/viewer/static')
 
 @app.route('/viewer/')
 @app.route('/viewer/<service>')
